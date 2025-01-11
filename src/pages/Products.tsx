df0d69fe03cfec187501c643f2d63659902c5873
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-10 22:46:30 -0400

    usando useLoader para hacer el get de products y mostrarlo en la UI

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,6 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import { getProducts } from "../services/ProductService";
+import { Product } from "../types";
+import ProductDetails from "../components/ProductDetails";
+
+export async function loader() {
+  console.log("Desde el loader");
+  const products = await getProducts();
+  console.log(products);
+
+  return products;
+}
 
 export default function Products() {
+  const products = useLoaderData() as Product[];
+  console.log(products);
+
   return (
     <>
       <div className="flex justify-between">
@@ -11,6 +25,24 @@ export default function Products() {
         >
           Agregar Producto
         </Link>
+      </div>
+
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <ProductDetails key={product.id} product={product} />
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,15 @@
-import { number, object, string } from "valibot";
+import { array, boolean, InferOutput, number, object, string } from "valibot";
 
 export const DraftProducSchema = object({
   name: string(),
   price: number(),
 });
+
+export const ProductSchema = object({
+  id: number(),
+  name: string(),
+  price: number(),
+  availability: boolean(),
+});
+export const ProductsSchema = array(ProductSchema);
+export type Product = InferOutput<typeof ProductSchema>;
