fc39c1df82f212fc210394bac0c3403e14f7cbe0
Juan Pablo Gonzales <juampi7237@gmail.com>
2025-01-11 20:41:41 -0400

    creado ruta para editar product usando link y usenavigate

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router-dom";
 import { Product } from "../types";
 import { formatCurrency } from "../utils";
 // import { formatCurrency } from "../utils";
@@ -9,12 +10,30 @@ type ProductDetailsProps = {
 export default function ProductDetails({ product }: ProductDetailsProps) {
   const isAvailable = product.availability;
 
+  const navigate = useNavigate();
+
   return (
     <tr className="border-b ">
       <td className="p-3 text-lg text-gray-800">{product.name}</td>
       <td className="p-3 text-lg text-gray-800">{formatCurrency(product.price)}</td>
       <td className="p-3 text-lg text-gray-800">{isAvailable ? "Disponible" : "No Disponible"}</td>
-      <td className="p-3 text-lg text-gray-800 "></td>
+      <td className="p-3 text-lg text-gray-800 ">
+        <div className="flex gap-2 items-center">
+          <Link
+            to={`productos/${product.id}/editar`}
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+          >
+            Editar Link
+          </Link>
+
+          <button
+            onClick={() => navigate(`/productos/${product.id}/editar`)}
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+          >
+            Editar Navigate
+          </button>
+        </div>
+      </td>
     </tr>
   );
 }
